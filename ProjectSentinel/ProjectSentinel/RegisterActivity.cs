4e9464c213e7f9da6d0af9650788220896ef4ca3
Antonio Radolović <anradol@unipu.hr>
2018-01-19 21:36:12 +0100

    Changed Register Form

--- ProjectSentinel/ProjectSentinel/RegisterActivity.cs
+++ ProjectSentinel/ProjectSentinel/RegisterActivity.cs
@@ -26,5 +26,30 @@ namespace ProjectSentinel
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterActivity_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
