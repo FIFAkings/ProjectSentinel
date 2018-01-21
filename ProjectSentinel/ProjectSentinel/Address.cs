aa76932bd44d1ec36984d63281f25722947e850c
Ivan Pešić <pelimir@gmail.com>
2018-01-21 23:10:43 +0100

    removed currently unnecessary imports

--- ProjectSentinel/ProjectSentinel/Address.cs
+++ ProjectSentinel/ProjectSentinel/Address.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mono.Data.Sqlite;
-using System.Data.SQLite;
 
 namespace ProjectSentinel
 {
--- ProjectSentinel/ProjectSentinel/User.cs
+++ ProjectSentinel/ProjectSentinel/User.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mono.Data.Sqlite;
-using System.Data.SQLite;
-using System.Security.Cryptography;
 
 namespace ProjectSentinel
 {
